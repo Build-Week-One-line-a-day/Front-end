7dd2f941f5756c779e14a4f49253bcefd64af6c2
bobbidigi <bobbidigi34@gmail.com>
2019-09-18 14:07:16 -0400

    add router

--- one-line/src/App.js
+++ one-line/src/App.js
@@ -1,13 +1,16 @@
 import React from 'react';
 import './App.css';
 import Login from './components/Login'
+import JournalList from './components/JournalList'
+import JournalForm from './components/JournalForm'
+import {Route} from 'react-router-dom'
 
 function App() {
   return (
     <div className="App">
-      <Login/>
-      <JournalList />
-      <JournalForm />
+      <Route path='/login' component={Login}/>
+      <Route path='/list' component={JournalList}/>
+      <Route path='/form' compoent={JournalForm}/>
     </div>
   );
 }
