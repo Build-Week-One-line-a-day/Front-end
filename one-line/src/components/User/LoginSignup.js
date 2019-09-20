4cdda88335f6d0b65a9f5e0e6a0694554132e787
Jesse-Tingle <jtingle0@gmail.com>
2019-09-20 00:39:21 -0400

    add svg image for home page, add main font family

--- one-line/src/components/User/LoginSignup.js
+++ one-line/src/components/User/LoginSignup.js
@@ -1,9 +1,13 @@
-import React from 'react'
-import {NavLink} from 'react-router-dom'
+import React from 'react';
+import {NavLink} from 'react-router-dom';
+// import styled from 'styled-components'
+import HomeImage from '../../img/HomeImage.svg'
 
 export default function LoginSignup() {
     return (
         <div>
+            <h1>One Line A Day Journal</h1>
+            <img width={400} src={HomeImage}/>
             <NavLink to='user-register'><button>Sign Up</button></NavLink>
             <NavLink to='user-login'><button>Log In</button></NavLink>
         </div>
