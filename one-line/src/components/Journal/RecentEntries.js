5cfc3a586b229bf5a6e0fd4ade9f939b67302d5a
bobbidigi <bobbidigi34@gmail.com>
2019-09-20 09:30:48 -0400

    add Entry component

--- one-line/src/components/Journal/RecentEntries.js
+++ one-line/src/components/Journal/RecentEntries.js
@@ -1,7 +1,7 @@
 import React, {useState, useEffect} from 'react'
 import axios from 'axios'
 import {NavLink} from 'react-router-dom'
-import { Card, Button } from 'semantic-ui-react'
+import styled from 'styled-components'
 import notes1 from '../../DesignFiles/notes1.svg'
 
 export default function RecentEntries() {
@@ -26,17 +26,38 @@ export default function RecentEntries() {
     },[])
 
     return (
-        <>
-            <NavLink to='/create'><Button primary>Add New</Button></NavLink>
-            <NavLink to='/full'><Button>Ten Year View</Button></NavLink>
-            <img width={100} src={notes1}/>
+        <ContainerDiv>
+            <img width={100} alt='notes' src={notes1}/>
+            <div class="btn-row">
+                <NavLink to='/create'><button primary>Add New</button></NavLink>
+                <NavLink to='/full'><button>Ten Year View</button></NavLink>
+            </div>
+           
             <h1>Recent Entries</h1>
             {entries.map((entry, index) =>{
-               return <Card key={index}>
-                        <Card.Content>{entry.name}</Card.Content>
-                    </Card> 
+               return <div key={index}>
+                        <p>06/06</p>
+                        <p>{entry.name}</p>
+                    </div> 
                
             })}
-        </>
+        </ContainerDiv>
     )
 }
+
+const ContainerDiv = styled.div`
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+
+    img{
+        margin: 0 auto;
+    }
+    h1{
+        font-family: 'Amatic SC',cursive;
+        font-size: 3rem;
+    }
+    .button-row {
+        display: flex;
+    }
+`
