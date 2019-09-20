3238a34262d5049bd2e69953c52711c433f3da60
bobbidigi <bobbidigi34@gmail.com>
2019-09-20 00:33:54 -0400

    add semantic-UI-react to recent entries

--- one-line/src/components/Journal/RecentEntries.js
+++ one-line/src/components/Journal/RecentEntries.js
@@ -1,6 +1,7 @@
 import React, {useState, useEffect} from 'react'
 import axios from 'axios'
 import {NavLink} from 'react-router-dom'
+import { Card, Button } from 'semantic-ui-react'
 
 export default function RecentEntries() {
     const [entries, setEntries] = useState([])
@@ -24,13 +25,16 @@ export default function RecentEntries() {
     },[])
 
     return (
-        <div>
-            <NavLink to='/create'><button>Add New</button></NavLink>
-            <NavLink to='/full'><button>Ten Year View</button></NavLink>
+        <>
+            <NavLink to='/create'><Button primary>Add New</Button></NavLink>
+            <NavLink to='/full'><Button>Ten Year View</Button></NavLink>
             <h1>Recent Entries</h1>
             {entries.map((entry, index) =>{
-               return <p key={index}>{entry.name}</p>
+               return <Card key={index}>
+                        <Card.Content>{entry.name}</Card.Content>
+                    </Card> 
+               
             })}
-        </div>
+        </>
     )
 }
