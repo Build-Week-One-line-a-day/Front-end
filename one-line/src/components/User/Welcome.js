440ef93e0b61f35485f5a173bdf008d4211d4076
bobbidigi <bobbidigi34@gmail.com>
2019-09-26 16:09:09 -0400

    add welcom message styles

--- one-line/src/components/User/Welcome.js
+++ one-line/src/components/User/Welcome.js
@@ -1,4 +1,5 @@
 import React from 'react'
+import styled from 'styled-components'
 
 export default function Welcome(props) {
 
@@ -11,8 +12,20 @@ export default function Welcome(props) {
 
 
     return (
-        <>
+        <ContainerDiv>
           <h1>{props.welcome}</h1>  
-        </>
+        </ContainerDiv>
     )
 }
+
+const ContainerDiv = styled.div`
+position: absolute !important;
+margin: 0;
+ h1{
+   background: #fff;
+   border-radius: 5px;
+   width: 100vw;   
+   padding: 5px;
+   margin: 0 auto;
+ }
+`
